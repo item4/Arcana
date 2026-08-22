37498c0828f80b81f3a7d05dd91c1c146a32c697
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2026-08-22 23:43:45 +0300

    Blacklisted villagers and bosses from Rod of Lyssa, disabled player stealing and altered the recipes. Fixes #176

---
+++ kubejs/server_scripts/reliquary/recipes.js
@@ -0,0 +1,12 @@
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
+ServerEvents.recipes(ATM => {
+    ATM.replaceInput({id: "reliquary:rod_of_lyssa"}, "minecraft:fishing_rod","#c:rods/allthemodium")
+})
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
