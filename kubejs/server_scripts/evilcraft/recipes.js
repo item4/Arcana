8c8380ec402d099523a759c9a3907befe1e794bd
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2026-08-22 23:40:38 +0300

    Add recipe for dark gem dust with ATO ore hammers. Fixes #63

---
+++ kubejs/server_scripts/evilcraft/recipes.js
@@ -0,0 +1,21 @@
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
+ServerEvents.recipes(ATM => {
+    ATM.custom({
+        type: "minecraft:crafting_shapeless",
+        category: "misc",
+        ingredients: [
+            { tag: "alltheores:ore_hammers" },
+            { item: "evilcraft:dark_gem" }
+        ],
+        result: { count: 1, id: "evilcraft:dark_gem_crushed" }
+    }).id('kubejs:evilcraft/dark_gem_crushed_from_hammer')
+})
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
