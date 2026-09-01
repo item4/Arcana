8ab5831fef712d327f018c5e7ccbcbd1ef698c2b
item4(Kim Jinsu) <item4@users.noreply.github.com>
2026-09-02 07:03:24 +0900

    [Cross-port] Explicitly mark Glyph of Nullify Defense as disabled (#219)
    * Disable Glyph of Nullify Defense
    
    * Hide Glyph of Nullify Defense from Recipe Viewer

---
+++ kubejs/client_scripts/hide/ars.js
@@ -0,0 +1,13 @@
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
+RecipeViewerEvents.removeEntriesCompletely(`item`, ATM => {
+    ATM.remove('ars_elemental:glyph_nullify_defense');
+})
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
